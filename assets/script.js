59509e929c91910ce13b3d78056ae5456ad52d8c
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-04-13 21:00:41 -0500

    got response back using fetch but spiderman is not there

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
-fetch(
-    "http://gateway.marvel.com/v1/public/characters?name=hulk&apikey=3a63bd6dec07e5572fe2f09b18064abe"
-)
+var inputEl = $('#input');
+var searchBtn = $('#search');
+
+
+
+searchBtn.on('click',function(){
+    var nameChar = inputEl.val()
+    var marvelUrl = "http://gateway.marvel.com/v1/public/characters?name="+nameChar+"&limit=100&apikey=3a63bd6dec07e5572fe2f09b18064abe"
+    fetch(marvelUrl)
     .then((res) => res.json())
     .then((data) => console.log(data))
+})

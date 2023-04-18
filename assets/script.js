fe58ddf4f27a00f15d3ef267f8fad1bcdaec308e
Abdulla <default@defaults-iMac.local>
2023-04-17 21:36:14 -0500

    imdb

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,12 @@
 var inputEl = $('#input');
 var searchBtn = $('#search');
+var selectEl = $('select');
 
-
-
-searchBtn.on('click',function(){
-    var nameChar = inputEl.val()
-    var marvelUrl = "http://gateway.marvel.com/v1/public/characters?name="+nameChar+"&limit=100&apikey=3a63bd6dec07e5572fe2f09b18064abe"
-    var imdbUrl = "https://imdb-api.com/en/API/SearchSeries/k_h6mhz1ew/" + nameChar
-    fetch(marvelUrl)
+selectEl.on('change', function() {
+  var nameChar = $(this).val();
+  var imdbUrl = "https://imdb-api.com/en/API/SearchSeries/k_h6mhz1ew/" + nameChar
+  
+  fetch(imdbUrl)
     .then((res) => res.json())
     .then((data) => console.log(data))
-    fetch(imdbUrl)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-})
+});

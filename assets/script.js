7b4c842c22dbda653905f60eb59cac82ceafa41e
Abdulla <default@defaults-iMac.local>
2023-04-17 19:12:39 -0500

    linksnbtn

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,11 @@ var searchBtn = $('#search');
 searchBtn.on('click',function(){
     var nameChar = inputEl.val()
     var marvelUrl = "http://gateway.marvel.com/v1/public/characters?name="+nameChar+"&limit=100&apikey=3a63bd6dec07e5572fe2f09b18064abe"
+    var imdbUrl = "https://imdb-api.com/en/API/SearchSeries/k_h6mhz1ew/" + nameChar
     fetch(marvelUrl)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    fetch(imdbUrl)
     .then((res) => res.json())
     .then((data) => console.log(data))
 })
